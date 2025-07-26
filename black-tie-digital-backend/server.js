cf520f9f8a4ed92cb0b883a7e05c0323dd696d94
Abhishek-Yadav17 <17abhishekadav@gmail.com>
2025-07-26 17:46:36 +0530

    added private mail

--- black-tie-digital-backend/server.js
+++ black-tie-digital-backend/server.js
@@ -11,7 +11,9 @@ app.post('/send', async (req, res) => {
   const { firstName, lastName, email, phone, message } = req.body;
 
   const transporter = nodemailer.createTransport({
-    service: 'gmail',
+    host: 'mail.privateemail.com',
+    port: 465,
+    secure: true,
     auth: {
       user: process.env.EMAIL,
       pass: process.env.PASS,
@@ -19,8 +21,9 @@ app.post('/send', async (req, res) => {
   });
 
   const mailOptions = {
-    from: email,
+    from: process.env.EMAIL,
     to: process.env.EMAIL,
+    replyTo: email,
     subject: 'New Contact Form Submission',
     text: `
       Name: ${firstName} ${lastName}
@@ -34,6 +37,7 @@ app.post('/send', async (req, res) => {
     await transporter.sendMail(mailOptions);
     res.status(200).json({ message: 'Email sent successfully' });
   } catch (err) {
+    console.error(err);
     res.status(500).json({ error: 'Failed to send email' });
   }
 });
